71a716804b778a508327e46ce564c90fc2a68c69
Tanzil <mtanzilislam1997@gmail.com>
2022-11-01 11:16:44 +0000

    fin

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,16 +5,38 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val)
+        if (!this.tail) {
+            this.head = newNode
+            this.tail = newNode
+        }
+
+        if(this.tail) {
+            this.tail.next = newNode
+        }
+
+        this.length++
+        return this.length
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
+        if(!this.head) {return undefined}
+
+        if(this.length === 1) {
+            this.head = null
+            this.tail = null
+        }
+
+        this.head = this.head.next
+        this.length--
 
         // Write your hypothesis on the time complexity of this method here
     }
